ffba232e15b94981a70ffbc1ba653c3e015617fb
monaalaa <monabindary@gmail.com>
2022-03-24 10:30:57 +0100

    Import Dotween and attack player

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using DG.Tweening;
 using Managers;
 using UnityEngine;
 
@@ -7,18 +7,46 @@ namespace Enemies
     public class EnemyController : MonoBehaviour
     {
         [SerializeField] private EnemyView view;
-        private void Start()
+        private GameObject _target;
+        private Vector3 _initialPosition;
+        private void Awake()
         {
             EventsManager.PlayerStartMoving += ResetPosition;
             EventsManager.PlayerStopMoving += AttackPlayer;
         }
+        private void Start()
+        {
+            _target = GameObject.FindWithTag(view.Model.PlayerTag);
+            _initialPosition = transform.position;
+        }
+        private void Update()
+        {
+            transform.LookAt(_target.transform);
+        }
+        
         private void AttackPlayer()
         {
-            throw new NotImplementedException();
+            var targetPosition = _target.transform.position;
+            var distance = Vector3.Distance (transform.position,targetPosition );
+            
+            if (distance < view.Model.AttackRange)
+            {
+                Vector3 dir = (this.transform.position - targetPosition).normalized;
+                targetPosition=  _target.transform.TransformPoint(dir * 5);
+                transform.DOMove(targetPosition,0.5f);
+            }
         }
+
         private void ResetPosition()
         {
-            throw new NotImplementedException();
+            Debug.Log("ResetPosition");
+            transform.DOMove(_initialPosition,0.5f);
+        }
+
+        private void OnDestroy()
+        {
+            EventsManager.PlayerStartMoving -= ResetPosition;
+            EventsManager.PlayerStopMoving -= AttackPlayer;
         }
     }
 }
--- Assets/Scripts/Enemies/Spawner/EnemiesSpawnerData.cs
+++ Assets/Scripts/Enemies/Spawner/EnemiesSpawnerData.cs
@@ -2,8 +2,8 @@
 {
     public class EnemiesSpawnerData
     {
-        public int NumberOfEnemies = 50;
-        public float SpawnXLimitation = 250;
-        public float SpawnZLimitation = 250;
+        public int NumberOfEnemies = 100;
+        public float SpawnXLimitation = 150;
+        public float SpawnZLimitation = 150;
     }
 }
