298f3f3dc73d93c350aa71a7ca05504f5653eec2
monaalaa <monabindary@gmail.com>
2022-03-24 10:41:00 +0100

    Make enemy wait to attack

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -1,4 +1,6 @@
-﻿using DG.Tweening;
+﻿using System.Collections;
+using System.Threading.Tasks;
+using DG.Tweening;
 using Managers;
 using UnityEngine;
 
@@ -9,6 +11,7 @@ namespace Enemies
         [SerializeField] private EnemyView view;
         private GameObject _target;
         private Vector3 _initialPosition;
+        private Coroutine _attackCoroutine;
         private void Awake()
         {
             EventsManager.PlayerStartMoving += ResetPosition;
@@ -31,15 +34,16 @@ namespace Enemies
             
             if (distance < view.Model.AttackRange)
             {
-                Vector3 dir = (this.transform.position - targetPosition).normalized;
-                targetPosition=  _target.transform.TransformPoint(dir * 5);
-                transform.DOMove(targetPosition,0.5f);
+                _attackCoroutine = StartCoroutine(WaitToAttack(targetPosition));
             }
         }
 
         private void ResetPosition()
         {
-            Debug.Log("ResetPosition");
+            if (_attackCoroutine != null)
+            {
+                StopCoroutine(_attackCoroutine);
+            }
             transform.DOMove(_initialPosition,0.5f);
         }
 
@@ -47,6 +51,14 @@ namespace Enemies
         {
             EventsManager.PlayerStartMoving -= ResetPosition;
             EventsManager.PlayerStopMoving -= AttackPlayer;
+        }
+
+        IEnumerator WaitToAttack(Vector3 targetPosition)
+        {
+            yield return new WaitForSeconds(0.8f);
+            Vector3 dir = (this.transform.position - targetPosition).normalized;
+            targetPosition=  _target.transform.TransformPoint(dir * 5);
+            transform.DOMove(targetPosition,0.5f);
         }
     }
 }
