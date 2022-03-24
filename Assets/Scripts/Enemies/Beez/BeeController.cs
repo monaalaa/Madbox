e03084248c02dca15d8c021bacdf8be5582609a5
monaalaa <monabindary@gmail.com>
2022-03-24 10:56:11 +0100

    Create Bee controller

---
+++ Assets/Scripts/Enemies/Beez/BeeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Enemies.Beez
+{
+    public class BeeController : EnemyController
+    { 
+        private void Update()
+        {
+            transform.LookAt(_target.transform);
+        }
+        protected override void AttackPlayer()
+        {
+            var targetPosition = _target.transform.position;
+            var distance = Vector3.Distance (transform.position,targetPosition );
+            
+            if (distance < view.Model.AttackRange)
+            {
+                _attackCoroutine = StartCoroutine(WaitToAttack(targetPosition));
+            }
+        }
+        protected override void ResetPosition()
+        {
+            base.ResetPosition();
+            
+            if (_attackCoroutine != null)
+            {
+                StopCoroutine(_attackCoroutine);
+            }
+        }
+        private IEnumerator WaitToAttack(Vector3 targetPosition)
+        {
+            yield return new WaitForSeconds(0.8f);
+            Vector3 dir = (this.transform.position - targetPosition).normalized;
+            targetPosition=  _target.transform.TransformPoint(dir * 5);
+            transform.DOMove(targetPosition,0.5f);
+        }
+    }
+}
--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -1,17 +1,15 @@
-﻿using System.Collections;
-using System.Threading.Tasks;
-using DG.Tweening;
+﻿using DG.Tweening;
 using Managers;
 using UnityEngine;
 
 namespace Enemies
 {
-    public class EnemyController : MonoBehaviour
+    public abstract class EnemyController : MonoBehaviour
     {
-        [SerializeField] private EnemyView view;
-        private GameObject _target;
+        public EnemyView view;
+        protected GameObject _target;
         private Vector3 _initialPosition;
-        private Coroutine _attackCoroutine;
+        protected Coroutine _attackCoroutine;
         private void Awake()
         {
             EventsManager.PlayerStartMoving += ResetPosition;
@@ -22,43 +20,19 @@ namespace Enemies
             _target = GameObject.FindWithTag(view.Model.PlayerTag);
             _initialPosition = transform.position;
         }
-        private void Update()
+        protected virtual void AttackPlayer()
         {
-            transform.LookAt(_target.transform);
+            
         }
         
-        private void AttackPlayer()
+        protected virtual void ResetPosition()
         {
-            var targetPosition = _target.transform.position;
-            var distance = Vector3.Distance (transform.position,targetPosition );
-            
-            if (distance < view.Model.AttackRange)
-            {
-                _attackCoroutine = StartCoroutine(WaitToAttack(targetPosition));
-            }
-        }
-
-        private void ResetPosition()
-        {
-            if (_attackCoroutine != null)
-            {
-                StopCoroutine(_attackCoroutine);
-            }
             transform.DOMove(_initialPosition,0.5f);
         }
-
         private void OnDestroy()
         {
             EventsManager.PlayerStartMoving -= ResetPosition;
             EventsManager.PlayerStopMoving -= AttackPlayer;
-        }
-
-        IEnumerator WaitToAttack(Vector3 targetPosition)
-        {
-            yield return new WaitForSeconds(0.8f);
-            Vector3 dir = (this.transform.position - targetPosition).normalized;
-            targetPosition=  _target.transform.TransformPoint(dir * 5);
-            transform.DOMove(targetPosition,0.5f);
         }
     }
 }
