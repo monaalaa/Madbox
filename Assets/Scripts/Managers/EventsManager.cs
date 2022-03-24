508eab3439437887a63fb9aa3ccd0eb1701ee04e
monaalaa <monabindary@gmail.com>
2022-03-24 08:54:31 +0100

    Create Events Manager

---
+++ Assets/Scripts/Managers/EventsManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Managers
+{
+    public static class EventsManager
+    {
+        public static Action PlayerStopMoving;
+        public static Action PlayerStartMoving;
+    }
+}
